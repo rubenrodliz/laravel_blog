55d563e19ed9953fcc21ad01eb8b1c58e286da41
Ruben <rubenrodliz@gmail.com>
2024-02-06 18:41:04 +0100

    Fix(Welcome): Modularizar header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,39 +14,8 @@
 </head>
 <body class="antialiased">
 <div class="flex flex-col min-h-screen">
-    <header class="flex h-14 items-center justify-between px-4 md:px-6">
-        <a class="flex items-center gap-2" href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" data-id="4">
-                <polyline points="16 18 22 12 16 6"></polyline>
-                <polyline points="8 6 2 12 8 18"></polyline>
-            </svg>
-            <span class="sr-only">
-                    Rubén DAW
-                </span>
-        </a>
-        @if (Route::has('login'))
-            <nav class="flex gap-4 sm:gap-6">
-                @auth
-                    <a href="{{ url('/dashboard') }}"
-                       class="text-sm font-medium hover:underline underline-offset-4">
-                        {{ __('Dashboard') }}
-                    </a>
-                @else
-                    <a href="{{ route('login') }}"
-                       class="text-sm font-medium hover:underline underline-offset-4">
-                        {{ __('Login') }}
-                    </a>
+    <x-header />
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                           class="text-sm font-medium hover:underline underline-offset-4">
-                            {{ __('Register') }}
-                        </a>
-                    @endif
-                @endauth
-            </nav>
-        @endif
-    </header>
     <main class="flex-1 flex flex-col justify-center min-h-full">
         <section class="w-full py-12 md:py-24 lg:py-32">
             <div class="px-4 md:px-6">
