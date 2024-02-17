6dee1303de17f6f598e8f07668d9afdb7b4df1e5
Ruben <rubenrodliz@gmail.com>
2024-02-17 19:19:10 +0100

    Feat(Page): Maquetación  inicial de la página y controlador

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Thread;
 
 class PageController extends Controller
 {
+    public function index() {
+        $threads = Thread::orderBy('id', 'DESC')->paginate();
+
+        return view('pages/index', compact('threads'));
+    }
     public function thread($thread) {
         return view('pages/thread', compact('thread'));
     }
