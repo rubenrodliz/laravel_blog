69d2d1c62eca2bc3a4f8f8758e9fbbf18cc18a77
rubenrodliz <rubenrodliz@gmail.com>
2024-02-17 12:55:30 +0100

    Feat(Thread): Integrar en template

---
+++ resources/views/pages/template.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-white">
+                @yield('title')
+            </h2>
+        </x-slot>
+        
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </x-app-layout>
+</body>
+
+</html>
--- resources/views/pages/thread.blade.php
+++ resources/views/pages/thread.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>{{ $thread }}</h1>
+@extends('pages/template')
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quia explicabo. Earum adipisci tenetur, cumque facilis saepe aliquam, sint asperiores perferendis iusto exercitationem enim consequuntur harum necessitatibus incidunt unde illum!Delectus maxime laborum nostrum ad, accusantium neque atque repudiandae cupiditate? Quibusdam aspernatur voluptatem consequatur adipisci, sapiente at sint odit laudantium numquam modi, ducimus quos. Id velit tempore consequatur quisquam earum.</p>
-</body>
-</html>
+@section('title', 'Thread')
+@section('description', 'This is the thread page')
+@section('content')
+    {{ $thread }}
+@endsection
