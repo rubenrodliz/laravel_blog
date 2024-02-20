3e3ecb3def0c7dbab4a6ff85833b7917c0d51df1
Ruben <rubenrodliz@gmail.com>
2024-02-20 16:27:54 +0100

    Fix(PageController): Optimización de consultas

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,12 +7,18 @@ use App\Models\Thread;
 class PageController extends Controller
 {
     public function index() {
-        $threads = Thread::orderBy('id', 'DESC')->paginate(5);
+        $threads = Thread::orderBy('id', 'DESC')
+            ->with('user')
+            ->withCount('comments')
+            ->paginate(5);
 
         return view('pages/index', compact('threads'));
     }
     public function thread(Thread $thread) {
-        $comments = $thread->comments()->paginate(5);
+        $comments = $thread
+            ->comments()
+            ->with('user')
+            ->paginate(5);
 
         return view('pages/thread', compact('thread', 'comments'));
     }
